39e333efa7686674dc9e6354b2fd302973d3acc2
Juan Antonio Zuloaga Mellino <jzuloaga@fi.uba.ar>
2022-12-09 05:03:14 -0300

    por fin 7

--- day7/day7.lisp
+++ day7/day7.lisp
@@ -1,4 +1,32 @@
 (ql:quickload "fiveam")
+(ql:quickload "draw-cons-tree")
+
+;;; Tree primitives
+
+(defun make-tree (data)
+    (cons (cons data nil) nil))
+
+(defun add-child (tree child) 
+    ;(nconc (car tree) child)
+    ;tree
+    (rplaca tree (append (car tree) child))
+    )
+
+(defmacro first-child (tree)
+    `(cdr (car ,tree)))
+
+(defmacro next-sibling (tree)
+    `(cdr ,tree))
+
+(defmacro data (tree)
+    `(car (car ,tree)))
+
+(defun make-root () (make-tree nil))
+
+(defun is-leaf (node)
+    (not (listp (cdr node))))
+
+;;; Processing
 
 (defun split-by-one-space (string)
     "Returns a list of substrings of string
@@ -10,33 +38,70 @@
           collect (subseq string i j)
           while j))
 
+(defmacro get-stack (tree-stack)
+    `(cdr ,tree-stack))
 
-(defun process-line (s stack)
-    (destructuring-bind (a b &optional c)  (split-by-one-space s)
-        (cond ((equal "$"  a) (process-command b c stack))
-              ((equal "dir" a) (add-directory (car stack) b))
-              ((parse-integer a :junk-allowed t) (add-file (car stack) b (parse-integer a)))
+
+(defmacro get-tree (tree-stack)
+    `(car ,tree-stack))
+
+(defun apply-pwd-node (f tree-stack)
+    (let* ((tree (get-tree tree-stack)) (node tree))
+         (dolist  (d (reverse (get-stack tree-stack)))
+             (setf node (do ((nodeb (first-child node) (next-sibling nodeb))) ((equal (data nodeb) d) nodeb))))
+         (funcall f node)
+         tree
+         )
+    )
+
+(defun apply-pwd-node (f tree-stack)
+(do* ((stack (reverse (get-stack tree-stack)) (cdr stack)) (tree (get-tree tree-stack)) (node tree))
+      ((not stack) (funcall f node) tree)
+      (setf node (do ((child (first-child node) (next-sibling child))) ((equal (data child) (car stack)) child)))))
+
+
+(let ((tree (make-tree nil)))
+    (apply-pwd-node (lambda (n) (add-child n (make-tree "b"))) (cons tree nil))
+    (apply-pwd-node (lambda (n) (add-child n (make-tree "c"))) (cons tree '("b")))
+    tree
+    )
+
+
+(let ((tree (make-tree nil)))
+    (apply-pwd-node (lambda (n) (add-child n (make-tree "c"))) (cons
+    (apply-pwd-node (lambda (n) (add-child n (make-tree "b"))) (cons tree nil)) '("b"))))
+
+
+(defun process-line (s tree-stack)
+    (destructuring-bind (a b &optional c)  (split-by-one-space s)        
+        (cond ((equal "$"  a) (rplacd tree-stack (process-command b c (get-stack tree-stack))))
+              ((equal "dir" a) (add-directory tree-stack b))
+              ((parse-integer a :junk-allowed t) (add-file tree-stack b (parse-integer a)))              
               )))
 
 (defun process-command (cmd arg stack)
     (cond 
      ((equal "cd" cmd) (cond 
-                        ((equal "/" arg) (loop                                                                                    
-                                          (when (not (getf (car stack) :name)) (return))
-                                          (pop stack)))
+                        ((equal "/" arg) (setf stack nil))
                         ((equal ".." arg) (pop stack))
-                        (t (push (find-if (lambda (d) (and (getf d :dir) (equal arg (getf d :name))))  (getf (car stack) :dlist) ) stack ))))
+                        (t (push arg stack))))
      ((equal "ls" cmd) nil)
     )
-    (car stack))
+    stack)
 
-(defun add-directory (pwd name) 
-    (push `(:name ,name :dir t :dlist ()) (getf pwd :dlist))
-    pwd)
+(defun add-directory (tree-stack name)
+    (apply-pwd-node (lambda (n) (add-child n (make-tree name))) tree-stack))
 
-(defun add-file (pwd name weight) 
-    (push `(:name ,name :dir nil :weight ,weight) (getf pwd :dlist))
-    pwd)
+
+(defun add-file (tree-stack name weight)
+    (apply-pwd-node (lambda (n) (add-child n (make-tree (cons name weight)))) tree-stack))
+
+(defun process-lines (lines tree-stack)
+    (dolist (l lines)
+        ;(print l)
+        ;(terpri)
+        (process-line l tree-stack))
+    tree-stack)
 
 (defun read-input (file)
   (with-open-file (in file)    
@@ -44,32 +109,132 @@
     (loop
         :for riv = (read-line in nil)
         :while riv
-        :do (process-line riv stack)
-        :finally (return stack)))))
+        :collect riv))))
 
 
-
-
-(fiveam:test test-change-directory
-    (fiveam:is  (equal `(:name ,nil :dir t) (process-line "$ cd /" `((:name ,nil :dir t))) ))
-    (fiveam:is  (equal `(:name ,nil :dir t) (process-line "$ cd /" `((:name a :dir t)(:name b :dir t)(:name ,nil :dir t)) )))
-    (fiveam:is  (equal '(:name b :dir t) (process-line "$ cd .." `((:name a :dir t)(:name b :dir t)(:name ,nil :dir t))) ))
-    (fiveam:is  (equal `(:name ,nil :dir t) (process-line "$ cd .." `((:name b :dir t)(:name ,nil :dir t))) ))
-    (fiveam:is  (equal `(:name "pepe" :dir t :dlist ((:name juan :dir f :weight 10))) (process-line "$ cd pepe" `((:name ,nil :dir t :dlist ((:name "pepe" :dir t :dlist ((:name juan :dir f :weight 10))))))))))
-
-(fiveam:test test-add-directory
-    (fiveam:is  (equal `(:name "base" :dir t :dlist ((:name "hijo" :dir t :dlist ())) ) (add-directory `(:name "base" :dir t :dlist ()) "hijo"))))
-
-(fiveam:test test-add-file
-    (fiveam:is  (equal `(:name "base" :dir t :dlist ((:name "hijo" :dir nil :weight 1000)) ) (add-file `(:name "base" :dir t :dlist ()) "hijo" 1000))))
-
-(fiveam:test test-parse-adds 
-    (fiveam:is  (equal `(:name ,nil :dir t :dlist ((:name "pepe" :dir t :dlist ()))) (process-line "dir pepe" `((:name ,nil :dir t :dlist ()))) ))
-    (fiveam:is  (equal `(:name ,nil :dir t :dlist ((:name "hijo" :dir nil :weight 1000))) (process-line "1000 hijo" `((:name ,nil :dir t :dlist ()))) ))
+(defun process-file (file)
+    (process-lines (read-input file) (cons (make-tree nil) nil))    
     )
 
-(fiveam:test test-input-first (fiveam:is-true t))
+;;; Testing
 
 
-(read-input "day7/test")
-;(fiveam:run!)
+(defparameter *test-tree* 
+    (reduce 'add-child ( list
+        (make-tree nil)
+                        
+        (reduce 'add-child ( list
+            (make-tree "a")
+            (reduce 'add-child ( list
+                (make-tree "e")
+                (make-tree (cons "i" 584))))
+            (make-tree (cons "f" 29116))
+            (make-tree (cons "g" 2557))
+            (make-tree (cons "h.lst" 62596))))
+                        
+        (make-tree (cons "b.txt" 14848514))
+        (make-tree (cons "c.dat" 8504156))
+                            
+        (reduce 'add-child ( list
+            (make-tree "d")
+            (make-tree (cons "j" 4060174))
+            (make-tree (cons "d.log" 8033020))
+            (make-tree (cons "d.ext" 5626152))
+            (make-tree (cons "k" 7214296))))
+        )))
+
+;(draw-cons-tree:draw-tree  *test-tree*)
+
+(fiveam:def-suite 7am-suite)
+(fiveam:in-suite 7am-suite)
+(fiveam:test test-change-directory
+    (fiveam:is  (equal nil (get-stack (process-line "$ cd /" (cons nil nil) ))))
+    (fiveam:is  (equal nil (get-stack (process-line "$ cd /" (cons nil '("e" "a")) ))))
+    (fiveam:is  (equal '("a") (get-stack (process-line "$ cd .."  (cons nil '("e" "a"))))))
+    (fiveam:is  (equal '("a" "d") (get-stack (process-line "$ cd .."  (cons nil '("e" "a" "d"))))))
+    (fiveam:is  (equal '("a") (get-stack (process-line "$ cd a"  (cons nil nil)))))
+    (fiveam:is  (equal '("b" "a") (get-stack (process-line "$ cd b"  (cons nil '("a"))))))    
+    )
+
+;(fiveam:test test-traverse
+;    (fiveam:is  (equal *test-tree* (get-pwd-node (cons *test-tree* nil))))
+;    (fiveam:is  (equal nil (get-pwd-node (cons *test-tree* '("e" "a"))))))
+
+(fiveam:test test-add-directory
+    (fiveam:is  (equal '((NIL ("a"))) (add-directory (cons (make-tree nil) nil) "a")))    
+    (fiveam:is  (equal '((NIL ("a" ("b")))) (let ((tree (make-tree nil))) 
+                                         (add-directory (cons tree nil) "a")
+                                         (add-directory (cons tree '("a")) "b")))))
+
+(fiveam:test test-add-file
+    (fiveam:is  (equal '((NIL (("a.txt" . 100)))) (add-file (cons (make-tree nil) nil) "a.txt" 100))))
+
+
+
+(fiveam:test test-parse-adds 
+    (fiveam:is  (equal '((NIL ("a"))) (process-line "dir a" (cons (make-tree nil) nil))))
+    (fiveam:is  (equal '((NIL ("a" ("b"))))  (process-line "dir b" (cons '((NIL ("a"))) '("a")))))
+    (fiveam:is  (equal '((NIL (("a.txt" . 100)))) (process-line "100 a.txt" (cons (make-tree nil) nil)))))
+
+(fiveam:test test-parse-lines
+    (fiveam:is  (equal '(((NIL ("a" ("e")))) "a") (process-lines '("$ cd /"
+                 "$ ls"
+                 "dir a"
+                 "$ cd a"
+                 "$ ls"
+                 "dir e") (cons (make-tree nil) nil))))
+
+    (fiveam:is  (equal '(((NIL ("a") (("b.txt" . 14848514)) (("c.dat" . 8504156)))))
+                      (process-lines '( "$ cd /" 
+                                       "$ ls" 
+                                       "dir a" 
+                                       "14848514 b.txt" 
+                                       "8504156 c.dat" ) (cons (make-tree nil) nil))))
+
+    (fiveam:is  (equal *test-tree* (get-tree (process-file "day7/test"))))
+    )
+
+
+(defun is-file (node)
+    (let ((datum (data node))) (and datum (not (atom datum)))))
+
+(defun is-folder (node)
+    (not (is-file node)))
+
+
+(defun get-weight (node)    
+        (if (is-folder node)             
+            (loop :for child = (first-child node) :then (next-sibling child) 
+                      :while child
+                      :as weight = (get-weight child)                      
+                      :when (is-folder child) :collect weight :into folderweight
+                      :sum (car weight) :into currentsum
+                      :finally (return (apply 'nconc (push (list currentsum) folderweight))))
+            (cons (cdr (data node)) nil )))
+            
+(defun resolve-first (ws)
+    (apply '+ (remove-if (lambda (w) (> w 100000)) (get-weight  ws))))
+
+(defun resolve-second (ws) 
+    (let ((weight (sort (get-weight  ws) '<))) 
+        (find-if (lambda (c) (> (+ c (- 70000000 (apply 'max weight))) 30000000)) weight)))
+
+
+(resolve-second (get-tree (process-file "day7/test")))
+
+(fiveam:test test-weight 
+    (fiveam:is  (equal '(100) (get-weight '((NIL (("a.txt" . 100)))))))
+    (fiveam:is  (equal '(100) (get-weight (first-child '((NIL (("a.txt" . 100))))))))
+    (fiveam:is  (equal '(48381165 94853 584 24933642) (get-weight  *test-tree*)))
+    (fiveam:is  (equal 95437 (resolve-first  (get-tree (process-file "day7/test")))))
+    (fiveam:is  (equal 1915606 (resolve-first  (get-tree (process-file "day7/input")))))
+    (fiveam:is  (equal 24933642 (resolve-second  (get-tree (process-file "day7/test")))))
+    (fiveam:is  (equal 1915606 (resolve-second  (get-tree (process-file "day7/input")))))
+    )
+
+
+
+
+(fiveam:run! '7am-suite)
+
+ 
