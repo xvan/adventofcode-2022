5bcae9f658a23cce151e1c654cf43d0065dfb3b9
Juan Antonio Zuloaga Mellino <jzuloaga@fi.uba.ar>
2024-12-04 02:23:27 -0300

    fuck

--- 2024/day3/day3.lisp
+++ 2024/day3/day3.lisp
@@ -10,6 +10,14 @@
   (with-open-file (in file)
   (read-line in nil)))
 
+(defun read-input2 (file)
+  (with-open-file (in file)
+    (loop :for riv = (read-char in nil) :while riv :collect riv)
+    )
+  )
+
+(read-input2 "")
+
 (defun str-to-list (str)
 (loop :for c :across str :collect c))
 
@@ -19,39 +27,50 @@
   ))
 
 
-(code-char 41)
-
-(defun parse (stack state &optional (acc 0) )
-  (+ acc (if stack 
-             (case (print state)
-              ((nil) (if (eq (car stack) #\m) (parse (cdr stack) 'm) (parse (cdr stack) nil)))
-              (m (if (eq (car stack) #\u) (parse (cdr stack) 'mu ) (parse stack nil)))
-              (mu (if (eq (car stack) #\l) (parse (cdr stack) 'mul ) (parse stack nil)))
-              (mul (if (eq (car stack) (code-char 40)) (parse (cdr stack) 'fd1) (parse stack nil)))
-              (fd1 (if (char-digit (car stack)) (parse stack 'd1) (parse stack nil)))
+(defun parse (stack state &optional (acc 0) (op1 0) (op2 0))
+  (if stack 
+             (case state
+              ((nil) (cond
+                  ((eq (car stack) #\m) (parse (cdr stack) 'm acc))
+                  (t (parse (cdr stack) nil acc)))
+                )
+        
+              (m (if (eq (car stack) #\u) (parse (cdr stack) 'mu acc ) (parse stack nil acc)))
+              (mu (if (eq (car stack) #\l) (parse (cdr stack) 'mul acc ) (parse stack nil acc)))
+              (mul (if (eq (car stack) (code-char 40)) (parse (cdr stack) 'fd1 acc) (parse stack nil acc)))
+              (fd1 (if (char-digit (car stack)) (parse stack 'd1 acc) (parse stack nil acc)))
               (d1 
                 (let* ( (v (char-digit (car stack)))
-                        (new-op (when v (+ v (* 10 acc)))))                
+                        (new-op (when v (+ v (* 10 op1)))))
                       (cond
-                          ((eq (car stack) #\,) (parse (cdr stack) 'fd2))                   
-                          ( (and new-op (< new-op 1000)) (parse (cdr stack) 'd1 new-op))
-                          (t (parse stack nil))
+                          ((eq (car stack) #\,) (parse (cdr stack) 'fd2 acc op1))                   
+                          ( (and new-op (< new-op 1000)) (parse (cdr stack) 'd1 acc new-op))
+                          (t (parse stack nil acc))
                           )))
-              (fd2 (if (char-digit (car stack)) (parse stack 'd2) (parse stack nil)))
+              (fd2 (if (char-digit (car stack)) (parse stack 'd2 acc op1) (parse stack nil acc)))
               (d2
-                (let* ( (v (char-digit (car stack)))
-                      (new-op (when v (+ v (* 10 acc)))))                
+              (let* ( (v (char-digit  (car stack)))
+                      (new-op (when v (+ v (* 10 op2)))))                
                     (cond
-                        ((eq (car stack) (code-char 40)) (parse (cdr stack) 'fd2))                   
-                        ( (and new-op (< new-op 1000)) (parse (cdr stack) 'd2 new-op))
-                        (t (parse stack nil))
+                        ((eq (car stack) (code-char 41))
+                          (progn (format t "mul(~d,~d) -> acc: ~d -> ~d~%" op1 op2 acc (+ acc (* op1 op2)))
+                          (parse (cdr stack) nil (+ acc (* op1 op2)))))
+                        ( (and new-op (< new-op 1000)) (parse (cdr stack) 'd2 acc op1 new-op))
+                        (t (parse stack nil acc))
                         )))
-              (otherwise (parse (cdr stack) nil))
-               ) 0
+              (otherwise (parse (cdr stack) nil acc))
+               ) acc
               )
-     ))
+    )
 
-(parse (str-to-list  "mul(1,2)") nil)
+
+
+
+(parse (str-to-list  "mul(123,123)") nil)
+(parse (str-to-list  *test_data*) nil)
+(parse (read-input2 "2024/day3/test_input") nil)
+
+(parse (read-input2 "2024/day3/input") nil)
 
 
 (defun boolean-to-integer (value)
@@ -61,33 +80,17 @@
   (apply #'+ (mapcar #'boolean-to-integer (mapcar #'is-safe data))))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun variations (test_line)
-  (loop for idx below (length test_line) collect
-          (loop :for i :in test_line 
-              :for jdx :from 0
-              :unless (= jdx idx) :collect i)
-          )  
-  )
-
-(defun is-safe-with-tolerance (test_line)
-    (some #'identity (mapcar #'is-safe (variations test_line))))
-
-(defun safe-reports-with-tolerance (data)
-  (apply #'+ (mapcar #'boolean-to-integer (mapcar #'is-safe-with-tolerance data))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(print (safe-reports (read-input "2024/day3/input")))
-(print (safe-reports-with-tolerance (read-input "2024/day3/input")))
+;;(print (safe-reports (read-input "2024/day3/input")))
+;;(print (safe-reports-with-tolerance (read-input "2024/day3/input")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(fiveam:test load-successfully
-  (fiveam:is (equal 
-              *test_data*
-              (read-input "2024/day3/test_input"))))
+; (fiveam:test load-successfully
+;   (fiveam:is (equal 
+;               *test_data*
+;               (read-input "2024/day3/test_input"))))
 
 
 (fiveam:test parser-end-condition
