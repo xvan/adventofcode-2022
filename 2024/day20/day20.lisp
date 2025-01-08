6ae8221bb1f8b8a5c3d030c996cbcc77fea2eaa9
Juan Antonio Zuloaga Mellino <jzuloaga@fi.uba.ar>
2025-01-08 01:40:17 -0300

    completely broken

--- 2024/day20/day20.lisp
+++ 2024/day20/day20.lisp
@@ -1,5 +1,8 @@
 (ql:quickload "Alexandria")
 
+(defun str-to-list (str)
+(loop :for c :across str :collect c))
+
 (defun list-to-2d-array (list)
   (make-array (list (length list)
                     (length (first list)))
@@ -100,71 +103,152 @@
     (find-exit-started graph end visited)
     ))
 
-(defun modify-map (mapa wall-coord next-coord)
-  (let ((mod-map (alexandria:copy-array mapa)))
-    (setf (aref-2d mod-map wall-coord) #\.)
-    (setf (aref-2d mod-map next-coord) #\#)
-    mod-map
-    ))
-
-(defun zero-if-nil (element) (if element element 0))
-
-(defun array-to-list (array)
-  (loop :for i :below (array-dimension array 0)
-        :collect (loop :for j :below (array-dimension array 1)
-                       :collect (aref array i j))))
-
-(defun print-map (mapa)
-  (format t "~%~{~{~a~}~%~}"  (array-to-list mapa))
-  mapa
-  )
-
-(defun alt-paths (coord next-coord mapa visited end)
-(loop
-           :for wall-coord :in (get-neighbour-walls coord mapa)
-           :for (distance path) := (find-exit-started
-                              (first (build-graph  (modify-map mapa wall-coord next-coord)))
-                              end
-                              (fourth (gethash coord visited)))
-           :collect (list coord wall-coord distance path)
-    ))
-
-(defun alt-paths2 (coord next-coord mapa start end)
-(loop
-           :for wall-coord :in (get-neighbour-walls coord mapa)
-           :for (distance path) := (find-exit
-                              (first (build-graph  (modify-map mapa wall-coord next-coord)))
-                           start   
-                           end
-                              )
-           :collect (list coord wall-coord distance path)
-    ))
-
 (defun solve-first (file)
  (let ((mapa (load-map file)))
   (destructuring-bind (f-graph start end) (build-graph mapa)
-    (destructuring-bind (f-length path success visited) (find-exit f-graph start end) 
-         ;(remove-if-not (lambda (x) (and (third x) (> (- f-length (third x)) 0)))
-            (loop
-              :for next-coord := end :then coord
-              :for coord :in path
-              :do (list success)
-              :append (alt-paths2 coord next-coord mapa start end))
-          ;:key (lambda (x) (third x)) 
-          ;)
-        ))))
-
-(untrace alt-paths)
-(untrace find-exit-started)
-
-(loop 
- :with catbag := (make-hash-table)
- :for d :in (mapcar (lambda (x) (and (third x) (- 84 (third x)))) (solve-first "2024/day20/test_input0"))
- :do (incf (gethash d catbag 0))
- :finally (maphash (lambda (k v) (format t "~a cheats save ~aps~%" v k)) catbag))
-
+    (destructuring-bind (f-length path success visited) (find-exit f-graph start end)
+        (list f-length path success visited) 
+        ;  (remove-if-not (lambda (x) (and x (> x 0)))
+        ;     (loop
+        ;       :for next-coord := end :then coord
+        ;       :for coord :in path
+        ;       :do (list success)
+        ;       :append (alt-paths2 coord next-coord mapa start end f-length))
+        ;   :key #'second)))
+         ))))
 
 (solve-first "2024/day20/test_input0")
+
+(solve-first "2024/day20/input")
+
+; (defun modify-map (mapa wall-coord next-coord)
+;   (let ((mod-map (alexandria:copy-array mapa)))
+;     (setf (aref-2d mod-map wall-coord) #\.)
+;     (setf (aref-2d mod-map next-coord) #\#)
+;     mod-map
+;     ))
+
+; (defun zero-if-nil (element) (if element element 0))
+
+; (defun array-to-list (array)
+;   (loop :for i :below (array-dimension array 0)
+;         :collect (loop :for j :below (array-dimension array 1)
+;                        :collect (aref array i j))))
+
+; (defun print-map (mapa)
+;   (format t "~%~{~{~a~}~%~}"  (array-to-list mapa))
+;   mapa
+;   )
+
+; (defun alt-paths (coord next-coord mapa visited end)
+; (loop
+;            :for wall-coord :in (get-neighbour-walls coord mapa)
+;            :for (distance path) := (find-exit-started
+;                               (first (build-graph  (modify-map mapa wall-coord next-coord)))
+;                               end
+;                               (fourth (gethash coord visited)))
+;            :collect (list coord wall-coord distance path)
+;     ))
+
+
+; (defun find-jump (coord path end)
+;     (loop
+;     :for next-coord := end :then current-coord
+;     :for current-coord :in path
+;     :when (equalp current-coord coord)
+;     :return next-coord
+;     )
+; )
+
+
+; (defun alt-paths2 (coord next-coord mapa start end f-length)
+; (loop
+;            :for wall-coord :in (get-neighbour-walls coord mapa)
+;            :for (distance path) := (find-exit
+;                               (first (build-graph  (modify-map mapa wall-coord next-coord)))
+;                            start   
+;                            end
+;                               )
+;            :collect (list (list wall-coord (find-jump wall-coord path end)) (and distance (- f-length distance)) path)
+;     ))
+
+
+
+; (defun solve-first (file)
+;  (let ((mapa (load-map file)))
+;   (destructuring-bind (f-graph start end) (build-graph mapa)
+;     (destructuring-bind (f-length path success visited) (find-exit f-graph start end)
+;         (list success visited) 
+;          (remove-if-not (lambda (x) (and x (> x 0)))
+;             (loop
+;               :for next-coord := end :then coord
+;               :for coord :in path
+;               :do (list success)
+;               :append (alt-paths2 coord next-coord mapa start end f-length))
+;           :key #'second)))
+;         ))
+
+; (solve-first "2024/day20/test_input0")
+
+; (mapcar (lambda (x) (list (first x) (third x))) (solve-first "2024/day20/test_input0"))
+
+; (defun hash-table-to-list (hash-table)
+;   (let ((result nil))
+;     (maphash (lambda (key value) (push (list key value) result))hash-table)
+;     result
+;     ))
+
+; (defun desambiguate (shortcuts)
+;   (let ((catbag (make-hash-table :test #'equalp)))
+;     (loop
+;      :for (shortcut delta) :in shortcuts
+;      :for current-delta := (gethash shortcut catbag nil)
+;      :do (when (or (not current-delta) (< current-delta delta))
+;              (setf (gethash shortcut catbag) delta))
+;      :finally (return (hash-table-to-list catbag))
+;      )
+;     )
+;   )
+
+; (defun count-shortcuts (shortcuts)
+;     (let ((all-entries nil))
+;  (loop 
+;  :with catbag := (make-hash-table)
+;  :for (shortcut delta) :in shortcuts
+;  :do (incf (gethash delta catbag 0))
+;  :finally (maphash (lambda (k v) (push (list k v) all-entries)) catbag)
+;  )
+;  (sort all-entries #'< :key #'first)
+;   ))
+
+; (count-shortcuts (desambiguate (solve-first "2024/day20/test_input0")))
+; (count-shortcuts (desambiguate (solve-first "2024/day20/input")))
+
+; (untrace alt-paths)
+; (untrace find-exit-started)
+
+; (let ((all-entries nil))
+;  (mapcar (lambda (x) 
+;                       (list (list (second x) (find-jump (second x) (fourth x) '(9 9)))
+;                       (and (third x) (- 84 (third x))))
+;                       ) (solve-first "2024/day20/test_input0"))
+;   )
+
+; (let ((all-entries nil))
+;  (loop 
+;  :with catbag := (make-hash-table)
+;  :for d :in (mapcar (lambda (x) 
+;                       (list (second x) (find-jump (second x) path))
+;                       (and (third x) (- 84 (third x)))                      
+;                       ) (solve-first "2024/day20/test_input0"))
+;  :do (incf (gethash d catbag 0))
+;  :finally (maphash (lambda (k v) (push (list k v) all-entries)) catbag))
+;  (sort all-entries #'< :key #'first)
+;   )
+
+
+
+
 ;(solve-first "2024/day2o/test_input0" '(6 6) 21)
  
 
